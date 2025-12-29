87582326794fce1b0fec9e4f9a402c1196a33dfe
Automate 6500 <42054743+automate6500@users.noreply.github.com>
2025-12-29 12:54:33 -0800

    Add files via upload

--- _plugins/baseurl_from_env.rb
+++ _plugins/baseurl_from_env.rb
@@ -2,6 +2,21 @@
 # This allows the site to work locally (with empty baseurl) and on GitHub Pages
 # (with repository-specific baseurl set via JEKYLL_BASEURL environment variable)
 
+module Jekyll
+  class BaseurlFromEnvGenerator < Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      if ENV['JEKYLL_BASEURL']
+        site.config['baseurl'] = ENV['JEKYLL_BASEURL']
+        Jekyll.logger.info "BaseurlFromEnv:", "Set baseurl to #{ENV['JEKYLL_BASEURL']}"
+      end
+    end
+  end
+end
+
+# Also register hooks as fallback
 Jekyll::Hooks.register :site, :after_init do |site|
   if ENV['JEKYLL_BASEURL']
     site.config['baseurl'] = ENV['JEKYLL_BASEURL']
